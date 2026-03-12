8788e604d91e8e9188ac9aaf3743bb3cf1fbc499
Quaid Tahir <quaidcodes@gmail.com>
2026-03-12 03:12:27 -0400

    color palette

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import {motion} from "motion/react";
+
 export default function Header() {
   return (
-    <nav>
-      <ul className="flex w-full flex-auto justify-evenly">
+    <nav className="m-2 flex justify-between ">
+      <h1 className="text-nowrap text-4xl">Quaid Tahir</h1>
+      <ul className="flex w-full flex-auto justify-end gap-4 text-2xl">
         <li>
           <a href="/">Home</a>
         </li>
@@ -18,6 +21,10 @@ export default function Header() {
 
         <li>
           <a href="/contact">Contact</a>
+        </li>
+        
+        <li>
+          <a href="/login">Sign in</a>
         </li>
       </ul>
     </nav>

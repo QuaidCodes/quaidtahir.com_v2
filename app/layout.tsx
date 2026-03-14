4be4e9f865da058fdcea365ef6c11d30713852eb
Quaid Tahir <quaidcodes@gmail.com>
2026-03-14 03:28:21 -0400

    header file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,18 @@
-"use client";
+
+import type { Metadata } from "next";
 
 import Header from "@/app/components/Header";
 
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Quaid Tahir Portfolio",
+  description: "Quaid Tahir - Software Engineer | Portfolio | Showcase | Resume",
+  keywords: ['software engineering', 'software', 'engineer', 'portfolio', 'sde'],
+  icons: {
+    icon: "path", // update this path for icon
+  }
+}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
